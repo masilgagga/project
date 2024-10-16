0d4bda590c3b5b89ae0f92e14dc1b5f5b37d69b6
zinee <zinee1@naver.com>
2024-10-16 17:29:49 +0900

    산책길 찾기 페이지 만드는 중

---
+++ header.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="./css/header.css" />
+<header>
+    <div class="head_wrap">
+        <div class="logo"><img src="./image/logo.png" alt="로고" /></div>
+        <div class="gnb">
+            <div><a href="">바르게 걷기</a></div>
+            <div><a href="">산책길 찾기</a></div>
+            <div><a href="">My 산책길</a></div>
+        </div>
+        <div class="login">로그인</div>
+        <div class="menu"><img src="./image/menu.png" alt="메뉴"></div>
+    </div>
+</header>
