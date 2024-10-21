eff138143adfe85fc607f252942c17345c7aeb24
MatinKim <kiki8428@naver.com>
2024-10-21 12:56:29 +0900

    리드미 참고

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
-$(function () {
-  // 사이드바
-  $(".account").click(function () {
-    $(".sidebar").toggleClass("toggle");
-
-    // 토글 상태에 따라 오버레이 표시
-    if ($(".sidebar").hasClass("toggle")) {
-      $(".overlay").fadeIn();
-    } else {
-      // 사이드바가 닫힐 때 opacity를 0으로 설정
-      $(".overlay").fadeOut();
-    }
-  });
-
-  // 오버레이 클릭 시 사이드바 닫기
-  $(".overlay").click(function () {
-    $(".sidebar").removeClass("toggle");
-    $(".overlay").fadeOut();
+document.addEventListener("DOMContentLoaded", () => {
+  let links = document.querySelectorAll(".walk_post");
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      window.location.href = "./walk_info.php";
+    });
   });
 });
