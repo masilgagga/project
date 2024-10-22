fa6399a7bd6ee57cf136a1ac44885cf7712f7059
wakuwakuJINI <synthi6785@gmail.com>
2024-10-22 14:22:07 +0900

    index,footer

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
 <link rel="stylesheet" href="./css/footer.css" />
 <footer>
     <div class="footer_wrap">
+        <div class="footer_logo">마실갈까</div>
+        <div>
+            <p>영진직업전문학교</p>
+            <p>Tel : 053-983-8877 Fax : 053-722-2423</p>
+            <p>사업자번호 : 502-95-07029</p>
+            <p>41166 대구 동구 화랑로 525 (용계동)</p>
+        </div>
     </div>
 </footer>
--- index.php
+++ index.php
@@ -1,36 +1,3 @@
-<?php
-// include "./config/const.php";
-
-// 데이터베이스 정보
-$host = '192.168.0.68';
-$db = 'zinee'; // 실제 데이터베이스 이름으로 변경
-$user = 'zinee'; // 사용자 이름
-$pass = 'tmakxmdnpqdoq5!'; // 비밀번호
-
-// 데이터베이스 연결
-$conn = new mysqli($host, $user, $pass, $db);
-
-// 연결 체크
-if ($conn->connect_error) {
-    die("연결 실패: " . $conn->connect_error);
-}
-
-// 페이지 번호 설정
-$items_per_page = 6; // 한 페이지에 보여줄 항목 수
-$current_page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-$offset = ($current_page - 1) * $items_per_page;
-
-// 총 데이터 수 가져오기
-$total_query = "SELECT COUNT(*) as total FROM data"; // 테이블 이름 확인
-$total_result = $conn->query($total_query);
-$total_row = $total_result->fetch_assoc();
-$total_items = $total_row['total'];
-$total_pages = ceil($total_items / $items_per_page); // 총 페이지 수
-
-// SQL 쿼리 (OFFSET 사용)
-$sql = "SELECT * FROM data LIMIT $items_per_page OFFSET $offset"; // 테이블 이름 확인
-$result = $conn->query($sql);
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -52,7 +19,26 @@ $result = $conn->query($sql);
         <?php include "header.php";?>
         <!-- PC모드 메인 -->
         <div class="mainContentsWrap">
-            <div class="mainContents"></div>
+            <div class="mainContetnsBtn">
+                <a href="">
+                    <img src="./image/index/main_dulyu.png" alt="두류">
+                </a>
+                <a href="">
+                    <img src="./image/index/main_beomeo.png" alt="범어">
+                </a>
+                <a href="">
+                    <img src="./image/index/main_apsan.png" alt="앞산">
+                </a>
+                <a href="">
+                    <img src="./image/index/main_sinam.png" alt="신암">
+                </a>
+                <a href="">
+                    <img src="./image/index/main_chimsan.png" alt="침산">
+                </a>
+            </div>
+            <div class="mainContents">
+                <img src="./image/index/main_back.png" alt="">
+            </div>
         </div>
         <!-- 상단 추천 산책로 섹션  -->
         <section class="sectionTop">
