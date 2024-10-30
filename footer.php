3abe03ecb12db30e017a78027f44b933a360671d
zinee <zinee1@naver.com>
2024-10-30 09:15:26 +0900

    푸터 수정
    푸터에 마실갈까 -> 마실가까 로 수정

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="./css/footer.css" />
 <footer>
     <div class="footer_wrap">
-        <div class="footer_logo"><div>마실갈까</div></div>
+        <div class="footer_logo"><div>마실가까</div></div>
         <div class="footer_text">
             <p>영진직업전문학교</p>
             <p>Tel : 053-983-8877 Fax : 053-722-2423</p>
