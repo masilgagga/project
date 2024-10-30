5452ff2a55ec15c820d692120b4d19fbb83dc38c
zinee <zinee1@naver.com>
2024-10-30 11:23:22 +0900

    회원삭제 시 내 산책길 정보 삭제

--- walk_admin_user_delete.php
+++ walk_admin_user_delete.php
@@ -4,13 +4,17 @@ include "./config/const.php";
 
 $userNum = $_POST["userNum"];
 
-// 관련 데이터 삭제 쿼리
-$deleteCommentsQuery = "DELETE FROM comment WHERE member_num = '{$userNum}';";
+// 회원 삭제 쿼리
 $deleteMemberQuery = "DELETE FROM member WHERE member_num = '{$userNum}';";
+// 댓글 삭제 쿼리
+$deleteCommentsQuery = "DELETE FROM comment WHERE member_num = '{$userNum}';";
+// 내산책길 삭제 쿼리
+$deleteLikeListQuery = "DELETE FROM member WHERE member_num = '{$userNum}';";
 
 // 데이터베이스에 쿼리 실행
-mysqli_query($DBCON, $deleteCommentsQuery);
 mysqli_query($DBCON, $deleteMemberQuery);
+mysqli_query($DBCON, $deleteCommentsQuery);
+mysqli_query($DBCON, $deleteLikeListQuery);
 
 mysqli_close($DBCON);
 
